ba8983112332e14c268e693863baff40289da4e5
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-07-28 09:18:49 +0300

    feat: (#problem3):Longest Substring Without Repeating Characters

---
+++ Medium/LengthOfLongestSubstring/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+public class Soluation
+{
+    public static int LengthOfLongestSubstring(string s)
+    {
+        HashSet<char> charSet = new HashSet<char>();
+        int left = 0, right = 0, maxLength = 0;
+
+        while (right < s.Length)
+        {
+            if (!charSet.Contains(s[right]))
+            {
+                charSet.Add(s[right]);
+                maxLength = Math.Max(maxLength, right - left + 1);
+                right++;
+            }
+            else
+            {
+                charSet.Remove(s[left]);
+                left++;
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void Main()
+    {
+        string inputStr = "bbbbb";
+        Console.WriteLine($"The length of the longest substring without repeating characters in '{inputStr}' is {LengthOfLongestSubstring(inputStr)}");
+    }
+}
+
