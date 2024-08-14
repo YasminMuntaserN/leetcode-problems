9885c5a425f9dc45c21b28407c57f570f7c259e5
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-08-14 08:42:46 +0300

    feat: add(#problem13)Length of Last Word

---
+++ Easy/LengthOfLastWord/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+public class Solution
+{
+    public static int LengthOfLastWord(string s)
+    {
+        // Trim the string to remove any leading or trailing whitespace
+        s = s.Trim();
+
+        // Find the index of the last space in the trimmed string
+        int lastSpaceIndex = s.LastIndexOf(' ');
+
+        // The last word starts after the last space, so calculate its length
+        int lengthOfLastWord = s.Length - lastSpaceIndex - 1;
+
+        return lengthOfLastWord;
+    }
+
+    public static void Main()
+    {
+        string s = "  fly me   to   the moon   ";
+        Console.WriteLine(LengthOfLastWord(s));
+
+        string s1 = "Hello World";
+        Console.WriteLine(LengthOfLastWord(s1));
+
+        string s2 = "luffy is still joyboy";
+        Console.WriteLine(LengthOfLastWord(s2));
+    }
+}
