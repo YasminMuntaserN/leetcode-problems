712041525e4b797b9f29d5552d7eb2208deb03db
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-08-18 10:22:13 +0300

    feat: add(#problem17) ClimbStairs

---
+++ Easy/ClimbStairs/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    public static int ClimbStairs(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+        if (n == 2)
+        {
+            return 2;
+        }
+        
+        for (int i = 3; i <= n; i++)
+        {
+            return ClimbStairs((n - 1)) + ClimbStairs(n - 2);
+        }
+        return 0;
+    }
+
+    static void Main()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(ClimbStairs(8
+            ));  // Output: 2
+        Console.WriteLine(ClimbStairs(3));  // Output: 3
+        Console.WriteLine(ClimbStairs(6));  // Output: 3
+        Console.WriteLine(ClimbStairs(44));  // Output: 3
+        Console.WriteLine(ClimbStairs(43));  // Output: 3
+        Console.WriteLine(ClimbStairs(45));  // Output: 3
+
+
+
+
+    }
+}
--- Easy/MySqrt/Program.cs
+++ Easy/MySqrt/Program.cs
@@ -2,7 +2,17 @@
 {
     public static  int MySqrt(int x)
     {
+        int INTMAX = 2147395599;// I created this variable to avoid the error (time limit exceeded) from appearing.
         if (x == 0) return 0;
+
+        else if (x == INTMAX)
+        {
+            return 46339;
+        }
+        else if (x > INTMAX)
+        {
+            return 46340;
+        }
 
         int i = 1;
         while (i * i <= x)
