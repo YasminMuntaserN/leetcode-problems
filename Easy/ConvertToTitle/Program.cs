64d0170f3db1562f0d59f6e2ea9a1980ce7c322f
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-08-24 09:17:16 +0300

    feat: add (#problem21) ConvertToTitle

---
+++ Easy/ConvertToTitle/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public static string ConvertToTitle(int columnNumber)
+    {
+
+        if (columnNumber == 0)
+        {
+            return "";
+        }
+
+        columnNumber--; // Adjust for 1-based indexing
+        char letter = (char)('A' + columnNumber % 26);
+        return ConvertToTitle(columnNumber / 26) + letter;
+    }
+    static void Main()
+    {
+        
+        Console.WriteLine(ConvertToTitle(701));
+    }
+}
