819cbc692602ec1a694ad6e46f137b1f97f9f6f6
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-04 08:49:53 +0300

    feat: add (#problem32)Add Digits

---
+++ Easy/Add Digits/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Solution
+{
+    /*
+      Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+       Example 1:
+       Input: num = 38
+       Output: 2
+       Explanation: The process is
+       38 --> 3 + 8 --> 11
+       11 --> 1 + 1 --> 2 
+       Since 2 has only one digit, return it.
+      */
+
+    public static int sumDigits(int num)
+    {
+        int sum = 0;
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    public static int AddDigits(int num)
+    {
+        int lastNum = sumDigits(num);
+        while (lastNum >= 10)
+        {  
+            lastNum = sumDigits(lastNum);
+        }
+        return lastNum;
+    }
+   
+    public static void Main()
+    {
+        Console.WriteLine(AddDigits(38));
+        Console.WriteLine(AddDigits(3));
+        Console.WriteLine( AddDigits(9875));
+        Console.WriteLine(AddDigits(123));
+
+    }
+}
