b5bc338a305b5e87d8e43aeef3568cf59402d6fe
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-21 11:55:57 +0300

    feat: add (#problem47)Contains Duplicate II

---
+++ Easy/Contains Duplicate II/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public static  bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        if (nums == null || nums.Length == 0) // Check outside the loop
+            return false;
+
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (map.ContainsKey(nums[i]))
+            {
+                int value = map[nums[i]];
+                int diff = i - value;
+                if (diff <= k)
+                {
+                    return true;
+                }
+            }
+            // Update the dictionary with the current index of nums[i]
+            map[nums[i]] = i;
+        }
+
+        return false;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(ContainsNearbyDuplicate([1, 2, 3, 1, 2, 3] ,2 ));
+    }
+}
