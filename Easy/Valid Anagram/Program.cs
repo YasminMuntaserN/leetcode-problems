a5fa0473ea7e0265efe933ce04f0fcef0d9bd8dc
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-22 09:03:24 +0300

    feat: add (#problem48) Valid Anagram

---
+++ Easy/Valid Anagram/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    /*
+     Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+     Example 1:
+     Input: s = "anagram", t = "nagaram"
+     Output: true
+     
+     Example 2:
+     Input: s = "rat", t = "car"
+     Output: false
+      */
+    public static  bool IsAnagram(string s, string t)
+    {
+        if(s.Length != t.Length) return false;
+
+        return string.Equals(new string(s.OrderBy(c => c).ToArray()), new string(t.OrderBy(c => c).ToArray()));
+
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(IsAnagram("anagram", "nagaram"));
+        Console.WriteLine(IsAnagram("rat", "car"));
+
+    }
+}
