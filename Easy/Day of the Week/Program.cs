18f051c7b9794194acf5027b19d9aa7f68503618
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-24 09:17:40 +0300

    feat: add (#problem49) Day of the Week

---
+++ Easy/Day of the Week/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic;
+
+public class Solution
+{
+    /*
+     Given a date, return the corresponding day of the week for that date.
+     The input is given as three integers representing the day, month and year respectively.
+     Return the answer as one of the following values {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
+      */
+    static void Main()
+    {
+        Console.WriteLine(DayOfTheWeek(31, 8, 2019)); // Output: Saturday
+        Console.WriteLine(DayOfTheWeek(18, 7, 1999)); // Output: Sunday
+        Console.WriteLine(DayOfTheWeek(15, 8, 1993)); // Output: Sunday
+    }
+
+    public static string DayOfTheWeek(int day, int month, int year)
+    {
+        return (new DateTime(year, month, day)).DayOfWeek.ToString();
+    }
+
+
+}
