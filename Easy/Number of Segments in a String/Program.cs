b7ec19d663a6df97dba77a2078db664e0811b331
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-07 10:43:12 +0200

    feat: add (#problem74) Number of Segments in a String

---
+++ Easy/Number of Segments in a String/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    /*
+     Given a string s, return the number of segments in the string.
+     A segment is defined to be a contiguous sequence of non-space characters.
+     
+     Example 1:
+     Input: s = "Hello, my name is John"
+     Output: 5
+     Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+     */
+    public static int CountSegments(string s)
+    {
+        if (String.IsNullOrWhiteSpace(s) ) return 0;
+        string []arr =s.Split(new char[] { ' ', '\t', '\n' });
+        
+
+        return arr.Count(el => !string.IsNullOrWhiteSpace(el));
+    }
+    public static void Main()
+    {
+        // Test cases
+        Console.WriteLine(CountSegments("Hello, world!")); 
+        Console.WriteLine(CountSegments("  Hello    world  "));
+        Console.WriteLine(CountSegments("OpenAI"));
+        Console.WriteLine(CountSegments("     ")); 
+        Console.WriteLine(CountSegments("")); 
+        Console.WriteLine(CountSegments("Hello, OpenAI! How's it going?")); 
+        Console.WriteLine(CountSegments("Hello,\nOpenAI\tis here!"));
+    }
+}
