ea4d58c1c216f599af1d27a001017bc66283dcd1
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-13 09:45:17 +0200

    feat: add (#problem79) License Key Formatting

---
+++ Easy/License Key Formatting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public static string LicenseKeyFormatting(string s, int k)
+    {
+        string Str = s.Replace("-", "").ToUpper();
+
+        StringBuilder result = new StringBuilder();
+
+        int count = 0;
+        for (int i = Str.Length - 1; i >= 0; i--)
+        {
+            result.Append(Str[i]);
+            count++;
+
+            if (count == k && i != 0)
+            {
+                result.Append("-");
+                count = 0;
+            }
+        }
+
+        return new string(result.ToString().Reverse().ToArray());
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(LicenseKeyFormatting("5F3Z-2e-9-w", 4)); 
+        Console.WriteLine(LicenseKeyFormatting("2-5g-3-J", 2));   
+    }
+}
