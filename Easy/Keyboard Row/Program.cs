66eaf238d10cf40d8433d87152f622ffe5fffb14
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-22 11:30:34 +0200

    feat: add (#problem83) Keyboard Row

---
+++ Easy/Keyboard Row/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+      // the first row consists of the characters "qwertyuiop",
+     // the second row consists of the characters "asdfghjkl", and
+    // the third row consists of the characters "zxcvbnm".
+    public static string[] FindWords(string[] words)
+    {
+        List<string> result = new  List<string>();
+        SortedSet<char> first = new SortedSet<char>("qwertyuiop");
+        SortedSet<char> second = new SortedSet<char>("asdfghjkl");
+        SortedSet<char> third = new SortedSet<char>("zxcvbnm");
+
+        foreach(string w in words)
+        {
+            string word =w.ToLower();   
+
+            if (word.All(c=> first.Contains(c))   ||
+                word.All(c => second.Contains(c)) ||
+                word.All(c => third.Contains(c)))
+            {
+                result.Add(w);
+            }
+        }
+        return result.ToArray();    
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(" , ", FindWords(["Hello", "Alaska", "Dad", "Peace"])));
+        Console.WriteLine(string.Join(" , ", FindWords(["omk"])));
+        Console.WriteLine(string.Join(" , ", FindWords(["adsdf", "sfd"])));
+
+    }
+}
