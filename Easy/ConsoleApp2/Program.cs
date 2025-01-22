74a9485042143eaf861b8e250f0a7bc24471b1ed
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2025-01-22 12:07:16 +0200

    feat: add (#problem84) Remove Letter To Equalize Frequency

---
+++ Easy/ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Timers;
+
+public class Program
+{
+    public static void Main()
+    {
+        // SelectionSort();
+        InsertionSort();
+    }
+
+    public static void SelectionSort()
+    {
+        List<int> list = new List<int>([64, 34, 25, 12, 22, 11, 90]);
+
+        Console.WriteLine($"List before sorting : {string.Join(" , ", list)} ");
+        for (int i = 0; i < list.Count - 1; i++)
+        {
+            int MaxIndex = list.IndexOf(list.Skip(i + 1).Max());
+
+            if (list[i] < list[MaxIndex])
+            {
+                (list[i], list[MaxIndex]) = (list[MaxIndex], list[i]);
+            }
+        }
+        Console.WriteLine($"List After sorting : {string.Join(" , ", list)} ");
+    }
+    public static void InsertionSort()
+    {
+        List<int> list = new List<int>([64, 34, 25, 12, 22, 11, 90]);
+
+        Console.WriteLine($"List before sorting : {string.Join(" , ", list)} ");
+        for (int i = 1; i < list.Count - 1; i++)
+        {
+            int index = i;
+            while (index > 0)
+            {
+                
+                int MaxIndex = list.IndexOf(list.Take(index + 1).Max());
+
+                if (list[index] < list[MaxIndex])
+                {
+                    (list[index], list[MaxIndex]) = (list[MaxIndex], list[index]);
+                }
+                index --;
+            }
+        }
+        Console.WriteLine($"List After sorting : {string.Join(" , ", list)} ");
+    }
+}
---
+++ Easy/Remove Letter To Equalize Frequency/Remove Letter To Equalize Frequency/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+You are given a 0-indexed string word, consisting of lowercase English letters. You need to select one index and remove the letter at that index
+from word so that the frequency of every letter present in word is equal.
+Return true if it is possible to remove one letter so that the frequency of all letters in word are equal, and false otherwise.
+
+Note:
+The frequency of a letter x is the number of times it occurs in the string.
+You must remove exactly one letter and cannot choose to do nothing.
+
+Example 1:
+Input: word = "abcc"
+Output: true
+Explanation: Select index 3 and delete it: word becomes "abc" and each character has a frequency of 1.
+ */
+public class Program {
+      public static bool EqualFrequency(string word)
+    {
+        for (int i = 0; i < word.Length; i++)
+        {
+            var str = word.Remove(i, 1);
+            var freq = GetFrequency(str);
+
+          //  Console.WriteLine($"After removing '{word[i]}': {str}: {string.Join(", ", freq.Select(kvp => $"{kvp.Key}: {kvp.Value}"))}");
+            var freqCount = freq.Values.GroupBy(v => v).ToDictionary(g => g.Key, g => g.Count()); 
+
+            if (freqCount.Count == 1)
+            {
+                return true;
+            }
+        }
+        return false;
+     }
+
+     private static Dictionary<char, int> GetFrequency(string word)
+     {
+         Dictionary<char, int> frequency = new Dictionary<char, int>();
+         foreach (char letter in word)
+         {
+             if (!frequency.ContainsKey(letter))
+                 frequency.Add(letter, 1);
+             else
+                 frequency[letter]++;
+         }
+         return frequency;
+     }
+    public static void Main()
+    {
+        Console.WriteLine(EqualFrequency("abcc")); 
+        Console.WriteLine(EqualFrequency("aazz")); 
+        Console.WriteLine(EqualFrequency("aaabbbccc")); 
+        Console.WriteLine(EqualFrequency("aabbccx")); 
+        Console.WriteLine(EqualFrequency("bac")); 
+    }
+}
