38a60abf632bc973ded4b24a0b84204c04b97781
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2025-01-28 08:42:55 +0200

    feat: add (#problem86) Perfect Number

---
+++ Easy/Perfect Number/Perfect Number/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    /*
+     A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+     Given an integer n, return true if n is a perfect number, otherwise return false.
+     */
+    public static bool CheckPerfectNumber(int num)
+    {
+        if (num <= 1) return false;
+        
+        var list = Enumerable.Range(1, (int)Math.Sqrt(num)).Where(i => num % i == 0).ToList();
+        int sum = list.Sum();
+        foreach (var i in list)
+            if (i != 1 && i != num / i) sum += num / i;
+        return sum == num;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($@" 7 => {CheckPerfectNumber(7)}");
+        Console.WriteLine($@" 28 => {CheckPerfectNumber(28)}");
+    }
+}
